8714ed4f921e3f1ae30838fb1e1899215437a1cd
FabioBoran <55750422+FabioBoran@users.noreply.github.com>
2020-05-19 16:23:32 +0200

    Database

---
+++ OvertimeDatabase/OvertimeDatabase/CreateDB.sql
@@ -0,0 +1,32 @@
+﻿DECLARE @databaseName sysname;  
+SET @databaseName = 'Overtime';  
+
+if db_id(@databaseName) is not null
+	BEGIN
+	   PRINT 'Database exists, nothing to do';
+	END
+ELSE
+	BEGIN
+		CREATE DATABASE Overtime;
+	END
+GO
+
+USE Overtime;
+GO  
+
+
+IF OBJECT_ID('dbo.Student', 'U') IS NOT NULL
+DROP TABLE dbo.Student;
+GO
+
+
+CREATE TABLE dbo.Student
+   (
+	Prename nvarchar(20),
+	Surname nvarchar(20),
+	Room nvarchar(8),
+	TimeStartM int,
+	TimeStartH int,
+	TimeEndM int,
+	TimeEndH int
+   ) ;
