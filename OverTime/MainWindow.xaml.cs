e784fbfa02de2f19433c672541a0d3ddf62de935
FelixJopkiewiczHTL <55581033+FelixJopkiewiczHTL@users.noreply.github.com>
2020-05-27 17:42:59 +0200

    na euda

---
+++ OverTime/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OverTime
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Window1 window1;
+
+        public MainWindow()
+        {
+            //DateTime x= DateTime.Now;
+            //  date.Text = x.ToShortDateString().ToString();
+            window1 = new Window1();
+            InitializeComponent();
+            window1.Show();
+            window1.GetStudents();
+        }
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            StreamWriter stream = new StreamWriter("data.txt",true);
+        
+            stream.WriteLine(firstname.Text);
+            stream.WriteLine(surname.Text);
+            stream.WriteLine(fromTo.Text);
+            stream.WriteLine(date.Text);
+            stream.WriteLine(pclass.Text);
+            stream.WriteLine();
+            stream.Dispose();
+            window1.GetStudents();
+            window1.Abgelaufen();
+           
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void fromTo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void date_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ OverTime/Window1.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using System.Threading;
+using System.Timers;
+using System.Windows.Navigation;
+
+namespace OverTime
+{
+    /// <summary>
+    /// Interaktionslogik für Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public static List<TextBlock> zulöschen;
+        public static List<TextBlock> ChildrenVor;
+        public static List<TextBlock> ChildrenNach;
+        public static List<TextBlock> ChildrenClass;
+        public static List<TextBlock> ChildrenDate;
+        public static List<TextBlock> ChildrenFromTo;
+        public Window1()
+        {
+            InitializeComponent();
+
+            PopulateGrids();
+
+            GetStudents();
+        }
+        private void PopulateGrids()
+        {
+            ChildrenVor = new List<TextBlock>();
+            for (int i = 0; i < 10; i++)
+            {
+                ChildrenVor.Add(new TextBlock());
+            }
+            foreach (var item in ChildrenVor)
+            {
+                Window1Vorname.Children.Add(item);
+            }
+
+            //
+            ChildrenNach = new List<TextBlock>();
+            for (int i = 0; i < 10; i++)
+            {
+                ChildrenNach.Add(new TextBlock());
+            }
+            foreach (var item in ChildrenNach)
+            {
+                Window1Nachname.Children.Add(item);
+            }
+            //
+
+            ChildrenClass = new List<TextBlock>();
+            for (int i = 0; i < 10; i++)
+            {
+                ChildrenClass.Add(new TextBlock());
+            }
+            foreach (var item in ChildrenClass)
+            {
+                Window1Class.Children.Add(item);
+            }
+            //
+            ChildrenDate = new List<TextBlock>();
+            for (int i = 0; i < 10; i++)
+            {
+                ChildrenDate.Add(new TextBlock());
+            }
+            foreach (var item in ChildrenDate)
+            {
+                Window1Date.Children.Add(item);
+            }
+            //
+            ChildrenFromTo = new List<TextBlock>();
+            for (int i = 0; i < 10; i++)
+            {
+                ChildrenFromTo.Add(new TextBlock());
+            }
+            foreach (var item in ChildrenFromTo)
+            {
+                Window1FromTo.Children.Add(item);
+            }
+        }
+
+        public void GetStudents()
+        {
+            StreamReader reader = new StreamReader("data.txt");
+            int i = 0;
+            try
+            {
+                while (!reader.EndOfStream)
+                {
+                    string vor, nach, date, fromto, clas;
+
+                    vor = reader.ReadLine();
+                    nach = reader.ReadLine();
+                    fromto = reader.ReadLine();
+                    date = reader.ReadLine();
+                    clas = reader.ReadLine();
+                    reader.ReadLine();
+                    // da müssen ein 
+                    if (new DateTime(int.Parse(date.Substring(6, 4)), int.Parse(date.Substring(3, 2)), 1 + int.Parse(date.Substring(0, 2))) == DateTime.Now)
+                    {
+                        ChildrenVor[i].Text  =  vor;
+                        ChildrenNach[i].Text = nach;
+                        ChildrenDate[i].Text = date;
+                        ChildrenFromTo[i].Text = fromto;
+                        ChildrenClass[i].Text = clas;
+                        i++;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                //Du hast nichts gesehen
+            }
+            reader.Dispose();
+        }
+        public void Abgelaufen()
+        {
+            GetStudents();
+        }
+    }
+}
